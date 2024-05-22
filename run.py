84baa8b097d2e9f20bb158f672d94a59d2f8e245
ssavi08 <sandro.savi0088@gmial.com>
2024-05-22 18:49:43 +0200

    commit

---
+++ run.py
@@ -0,0 +1,74 @@
+# app.py
+import streamlit as st
+import cv2
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+
+# Load the pre-trained model and Haar cascade classifier
+face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+classifier = load_model("model.h5")
+
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+
+# Function to process video frames and predict emotions
+def predict_emotions(frame):
+    labels = []
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi_gray]) != 0:
+            roi = roi_gray.astype('float') / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label = emotion_labels[prediction.argmax()]
+            label_position = (x, y - 10)
+            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, 'No Faces', (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    return frame
+
+
+# Function to display the video stream
+def show_camera():
+    cap = cv2.VideoCapture(0)
+    video_placeholder = st.empty()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.write("Failed to capture video")
+            break
+
+        frame = predict_emotions(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        video_placeholder.image(frame, channels="RGB")
+
+        if st.session_state.stop_camera:
+            break
+
+    cap.release()
+
+
+# Main function to set up Streamlit interface
+def main():
+    st.title("Predikcija osnovnih emocija prema izrazu lica")
+    st.write("Projekt kolegija Primjenjeno strojno učenje")
+    st.write("Made by: Savi & Filip")
+
+    if st.button("Pokreni kameru", key="start"):
+        st.session_state.stop_camera = False
+        show_camera()
+
+
+if __name__ == "__main__":
+    main()
